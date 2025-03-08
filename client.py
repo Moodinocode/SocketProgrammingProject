84f91a89b192347078a61b28e8bbc016647c7505
Moodinocode <mehdi21092005@gmail.com>
2025-03-08 03:08:38 +0200

    basic flask page

--- client.py
+++ client.py
@@ -4,10 +4,22 @@ PORT = 8080
 ServerIP = "0.0.0.0" # we can also put local host 
 
 
-def initiateClient():
+def initiateClient(command): #command should be sent from the flask web app
   clientSocket = socket(AF_INET,SOCK_STREAM)
   clientSocket.connect((ServerIP,PORT))
   #client functionality
+  # Step 1 take from client 3 parameters
+  # (command, file name,file) 
+  # Command are file operations uplaod/downlaod/list
+  # filename should be either the name of the file that is being uploaded or the one being requrested to be dowloaded
+  # file should be the file being uploaded
+  if command == 1:
+    print("upload")
+  elif command == 2:
+    print("download")
+  elif command == 3:
+    print("list")
+
   clientSocket.close()
 
 
--- flask_app.py
+++ flask_app.py
@@ -1,1 +1,24 @@
 #web interface
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '''
+      <html>
+        <head>
+          <title>File Sharing</title>
+        </head>
+        <body>
+          <h1>Hello, World!</h1>
+          <button>Uplaod</button>
+          <button>Download</button>
+          <button>List</button>
+          <p>Welcome to my Flask application.</p>
+        </body>
+      </html>
+    '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
